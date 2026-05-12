737d432ee1a9d8556005421bdf6c0f5fa73d049e
lzhenwei <lzhenwei76@gmail.com>
2026-05-12 18:55:35 +0200

    Add files via upload

---
+++ HydrogenMInstaller/HydrogenMInstallerTests/HydrogenMInstallerTests.swift
@@ -0,0 +1,19 @@
+//
+//  HydrogenMInstallerTests.swift
+//  HydrogenMInstallerTests
+//
+//  Created by Zhenwei on 12.05.26.
+//
+
+import Testing
+@testable import HydrogenMInstaller
+
+struct HydrogenMInstallerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
